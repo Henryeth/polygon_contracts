315a82a7321e2bb84f6f143f9cb4b8712486871a
Hung Le <hung.le@roboworld.io>
2022-10-13 12:05:08 +0700

    Add burnable to NFT

--- URANFT.sol
+++ URANFT.sol
@@ -5,8 +5,9 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
-contract NFT is ERC721URIStorage, ERC721Enumerable {
+contract NFT is ERC721URIStorage, ERC721Enumerable, ERC721Burnable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     address contractAddress;
